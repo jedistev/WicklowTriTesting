09d61a35fde53fde17d2906b843db6878a3833c8
steven smith <jedistev@gmail.com>
2017-05-15 11:26:40 +0100

    Wicklow tri Automation test

---
+++ WicklowTriProject/WicklowTriAllPages.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class WicklowTrIProject
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://www.wicklowtri.com/index.php";
+            verificationErrors = new StringBuilder();
+        }
+        
+        
+        [Test]
+        public void TheWicklowTrIProjectTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/index.php");
+            driver.FindElement(By.XPath("(//a[contains(text(),'Home')])[2]")).Click();
+            driver.FindElement(By.CssSelector("a..dropdown-toggle")).Click();
+            driver.FindElement(By.CssSelector("ul.mega-nav.level1 > li > a")).Click();
+            driver.FindElement(By.CssSelector("a..dropdown-toggle")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Committee & Coaches')])[2]")).Click();
+            driver.FindElement(By.CssSelector("a..dropdown-toggle")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Club Training Sessions')])[2]")).Click();
+            driver.FindElement(By.CssSelector("a..dropdown-toggle")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Contact')])[3]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Events')])[2]")).Click();
+            driver.FindElement(By.CssSelector("div.ca-evp29.te")).Click();
+            driver.FindElement(By.CssSelector("div.ca-evp43.te > span.te-s")).Click();
+            driver.FindElement(By.CssSelector("div.ca-evp49.te > span.te-s")).Click();
+            driver.FindElement(By.Id("bubbleClose:1")).Click();
+            driver.FindElement(By.XPath("//header[@id='t3-mainnav']/div/nav[2]/div/ul/li[3]/a/em")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Base2Race Harbourman Triathlon')])[2]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Media')])[2]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'News')])[3]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Members')])[3]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Search')])[2]")).Click();
+            driver.FindElement(By.Id("search-searchword")).Clear();
+            driver.FindElement(By.Id("search-searchword")).SendKeys("Cork");
+            driver.FindElement(By.Name("Search")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Training')])[14]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Members')])[3]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Forum')])[16]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Members')])[3]")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'WTC Ironman')])[2]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ WicklowTriProject/WicklowTriWebsite.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace WicklowTriProject
+{
+    [TestClass]
+    public class WicklowTriWebsite
+    {
+        static IWebDriver driverFF;
+       
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverFF = new FirefoxDriver();
+
+        }
+
+        [TestMethod]
+        public void TestWicklowTri()
+        {
+            driverFF.Navigate().GoToUrl("http://www.wicklowtri.com");
+        }
+
+        [TestMethod]
+        public void ForumPages()
+        {
+            driverFF.Navigate().GoToUrl("http://www.wicklowtri.com/index.php/forum");
+        }
+
+        [TestMethod]
+        public void WrongPasswordForumPages()
+        {
+            driverFF.FindElement(By.Name("username")).SendKeys("Stevenpetersmi");
+            driverFF.FindElement(By.Name("password")).SendKeys("password");
+            driverFF.FindElement(By.ClassName("kbutton")).SendKeys(Keys.Enter);
+
+        }
+
+        [TestMethod]
+        public void CorrectPasswordForumPages()
+        {
+            driverFF.Navigate().GoToUrl("http://www.wicklowtri.com/index.php/forum");
+            driverFF.FindElement(By.Name("username")).SendKeys("StevenAuto");
+            driverFF.FindElement(By.Name("password")).SendKeys("autotestpassword");
+            driverFF.FindElement(By.ClassName("kbutton")).SendKeys(Keys.Enter);
+
+        }
+
+        [TestMethod]
+        public void logoutForumPages()
+        {
+            
+            driverFF.FindElement(By.ClassName("kbutton")).SendKeys(Keys.Enter);
+
+
+        }
+
+        [TestMethod]
+        public void Pages()
+        {
+            driverFF.Navigate().GoToUrl("http://www.wicklowtri.com/index.php/2015-04-07-20-42-40/news");
+        }
+
+        [TestMethod]
+         public void refresh()
+         {
+             driverFF.Navigate().Refresh();
+         }
+
+
+        // [AssemblyCleanup]
+        //  public static void ClosedWebSite()
+        //  {
+        //    driverFF.Quit();
+        //   }
+    }
+}
